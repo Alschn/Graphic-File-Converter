47f53fb6d14babde122bdf459a8115317e30aa3b
Adam Piotr Lisichin <a.lisichin@stud.elka.pw.edu.pl>
2020-03-31 21:59:49 +0200

    Move processImage from Converter to Rotation

--- graphic-file-converter/Converter.cpp
+++ graphic-file-converter/Converter.cpp
@@ -7,19 +7,3 @@ void Converter::saveImage(const std::string& path) const
 	this->newImage->save(path);
 }
 
-void Converter::proccessImage(int angle)
-{
-    Rotation.angle = angle;
-    for(const auto pair:Rotation.map)
-    {
-        auto old_x = pair.first.first;
-        auto old_y = pair.first.second;
-        auto new_x = pair.second.first;
-        auto new_y = pair.second.second;
- 
-        unsigned char pixels[3];
-
-        this->oldImage->getPixel(old_x, old_y, pixels);
-        this->newImage->putPixel(new_x, new_y, pixels);
-    }
-}
--- graphic-file-converter/rotation.cpp
+++ graphic-file-converter/rotation.cpp
@@ -45,3 +45,20 @@ void Rotation::createMap()
     }
     }
 };
+
+void Rotation::proccessImage(int angle)
+{
+    Rotation.angle = angle;
+    for(const auto pair:Rotation.map)
+    {
+        auto old_x = pair.first.first;
+        auto old_y = pair.first.second;
+        auto new_x = pair.second.first;
+        auto new_y = pair.second.second;
+ 
+        unsigned char pixels[3];
+
+        this->oldImage->getPixel(old_x, old_y, pixels);
+        this->newImage->putPixel(new_x, new_y, pixels);
+    }
+}
