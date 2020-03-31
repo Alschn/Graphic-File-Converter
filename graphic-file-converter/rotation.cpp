a4f12422c206850da5daf019c4093cde134fbd12
Adam Piotr Lisichin <a.lisichin@stud.elka.pw.edu.pl>
2020-03-31 16:11:25 +0200

    commit Rotation, Converter

--- graphic-file-converter/rotation.cpp
+++ graphic-file-converter/rotation.cpp
@@ -1,1 +1,66 @@
-#include "rotation.h"
+﻿#include "rotation.h"
+#include <cmath>
+
+void Rotation::createMap()
+{   
+    if (angle % 90 != 0)
+    {
+        // raise exception
+    }
+    if (angle > 360)
+    {
+        int multiple = angle / 360;
+        angle = angle - multiple*360;
+    }
+    if (angle != 360 or angle != 0)
+    {
+    const double pi = std::acos(-1);
+    double deg = angle * pi / 180;
+
+    for (int i = 0; i < this->oldImage->height; i++)
+    {
+        for (int j = 0; j < this->oldImage->width; j++)
+        {
+            // rotation matrix
+            int x_n = j * std::cos(-deg) -  i * std::sin(-deg);
+            int y_n = j * std::sin(-deg) + i * std::cos(-deg);
+            // translation vector
+            switch (angle)
+            {
+            case 90:
+                y_n += this->oldImage->width;
+                break;
+            case 180:
+                y_n += this->oldImage->width;
+                y_n += this->oldImage->height;
+                break;
+            case 270:
+                x_n += this->oldImage->height;
+                break;
+            }
+           
+            map.emplace(std::make_pair(std::make_pair(j, i), std::make_pair(x_n, y_n)));
+           
+        }
+    }
+    }
+};
+
+
+// to jest źle
+void Rotation::proccessImage(int angle)
+{
+    this->angle = angle;
+    for(const auto pair:map)
+    {
+        auto old_x = pair.first.first;
+        auto old_y = pair.first.second;
+        auto new_x = pair.second.first;
+        auto new_y = pair.second.second;
+ 
+        unsigned char pixels[3];
+
+        this->oldImage->getPixel(old_x, old_y, pixels);
+        this->newImage->putPixel(new_x, new_y, pixels);
+    }
+};
--- graphic-file-converter/rotation.h
+++ graphic-file-converter/rotation.h
@@ -1,7 +1,13 @@
 #pragma once
 #include "converter.h"
+#include <map>
 
 class Rotation: public Converter
 {
+    private:
+    std::map <std::pair<int, int>, std::pair<int, int>> map;
+    void createMap();
 
+    public:
+    int angle;
 };
