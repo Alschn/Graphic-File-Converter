5376fbbab6f981af60e89f3464dfd5801b0a9839
Wojciech Nowicki <wnowicki16@gmail.com>
2020-04-07 23:28:04 +0200

    Registration with regex, "intelligent" regex, executeAction, showHelp improvement

--- graphic-file-converter/user_interface.cpp
+++ graphic-file-converter/user_interface.cpp
@@ -3,36 +3,90 @@
 #include <map>
 #include <string>
 #include <regex>
+#include <sstream>
+#include <vector>
+#include "image.h"
+#include "converter.h"
 
-std::map<std::string, Converter*> UserInterface::conversions_map;
-std::map <std::string, std::string> UserInterface::help_map;
+std::map <const std::string, Converter*> UserInterface::conversions_map;
+std::map <const std::string, const std::string> UserInterface::help_map;
+std::map <const std::string, std::regex> UserInterface::regex_map;
+std::map <const std::string, int> UserInterface::number_of_command_arguments_map;
 
-void UserInterface::registerAction(const std::string& command_name, const std::string& command_explanation, Converter *conversion)
+void UserInterface::registerAction(const std::string& command_name, const std::string& command_explanation, Converter* conversion, std::regex command_regex, int number_of_arguments)
 {
-    conversions_map.emplace(command_name, conversion);
-    help_map.emplace(command_name, command_explanation);
+	conversions_map.emplace(command_name, conversion);
+	help_map.emplace(command_name, command_explanation);
+	regex_map.emplace(command_name, command_regex);
+	number_of_command_arguments_map.emplace(command_name, number_of_arguments);
 }
 
 
 void UserInterface::display()
 {
-    Image image;
-    std::cout << "Welcome to file converter!" << std::endl;
-    std::cout << "Type command or --help to get commands examples" << std::endl;
-    std::string command;
-    std::getline(std::cin, command);
-	std::regex reg(R"(^load \S+$)");
-	std::smatch match;
-    std::regex_search(command, match, reg);
-    for(auto x: match)
+    std::cout << "Welcome to Graphic File Converter" << std::endl;
+    std::cout << "Please enter your command:" << std::endl;
+    for (;;)
     {
-        std::cout << x << std::endl;
+        std::string command;
+        std::getline(std::cin, command);
+        std::regex choose_route_regex(R"(^converter +load +[\S] +$)");
+        std::smatch choose_route_regex_matches;
+        std::regex_search(command, choose_route_regex_matches, choose_route_regex);
+    	if (choose_route_regex_matches.ready() == true)
+    	{
+            std::cout << "Sciezka za raczke!" << std::endl;
+        }
+        else
+        {
+            std::smatch command_match;
+        	for(auto command_name:regex_map)
+	        {
+                std::regex_search(command, command_match, command_name.second);
+        		if(command_match.ready()==true)
+        		{
+                    std::stringstream command_ss(command);
+                    std::vector<std::string> splitted;
+                    std::string buf_string;
+        			while(command_ss>>buf_string)
+        			{
+                        splitted.push_back(buf_string);
+        			}
+                    std::vector<int> arguments;
+                    for (auto m : splitted)
+                    {
+                        try
+                        {
+                            int argument = std::stoi(m);
+                            arguments.push_back(argument);
+                        }
+                        catch (...)
+                        {
+                            ;
+                        }	
+                    }        				
+
+
+        			
+        		}
+                else
+                {
+                    showHelp();
+                }
+	        }
+        }
+
+
     }
 }
 
-void UserInterface::executeAction(const std::string& command)
+void UserInterface::executeAction(const std::string& command, const std::string& path, const std::string& out_path)
 {
-   
+    Image image(path, 1, ImageMode::ReadFromBMP, ColorDepth::bpp24);
+	Converter* conversion = conversions_map[command];
+    conversion->loadImage(&image);
+    //conversion.processImage(arguments);
+	conversion->saveImage(out_path);
 }
 
 void UserInterface::registerHelp(const std::string& command_name, const std::string& explanation)
@@ -42,7 +96,10 @@ void UserInterface::registerHelp(const std::string& command_name, const std::str
 
 void UserInterface::showHelp()
 {
-    std:: cout << "Possible commands: " << std::endl;
+    std::cout << "Command structure:" << std::endl;
+    std::cout << "converter command_name arguments 'in_path' ['out_path']" << std::endl;
+    std::cout << "[] - possible, not needed, notice that not all functions need arguments" << std::endl;
+	std:: cout << "Possible commands: " << std::endl;
     for (auto explanation : help_map)
     {
         std::cout << explanation.first << "    " << explanation.second << std::endl;
--- graphic-file-converter/user_interface.h
+++ graphic-file-converter/user_interface.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <map>
-
+#include <regex>
 #include "converter.h"
 
 
@@ -9,12 +9,14 @@ class UserInterface
 {
 
 public:
-	static std::map <std::string, Converter*> conversions_map;
-	static std::map <std::string, std::string> help_map;
-
-	void registerAction(const std::string& command_name, const std::string& command_explanation, Converter* conversion);
+	static std::map <const std::string, Converter*> conversions_map;
+	static std::map <const std::string, const std::string> help_map;
+	static std::map <const std::string, std::regex> regex_map;
+	static std::map <const std::string, int> number_of_command_arguments_map;
+	
+	void registerAction(const std::string& command_name, const std::string& command_explanation, Converter* conversion, std::regex command_regex, int number_of_arguments);
 	void display();
-	void executeAction(const std::string& command);
+	void executeAction(const std::string& command, const std::string& path, const std::string& out_path);
 	void showHelp();
 	void registerHelp(const std::string& command_name, const std::string& explanation);
 };
