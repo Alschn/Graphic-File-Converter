462da26969a3e9d5e7b2a71141ac0d126985f6df
Adam Lisichin <01149732@pw.edu.pl>
2020-04-26 16:08:22 +0200

    Initial scaling algorithm implementation

--- graphic-file-converter/rescaler.cpp
+++ graphic-file-converter/rescaler.cpp
@@ -1,11 +1,32 @@
 #include "rescaler.h"
 
-double Rescaler::lerp(double s, double e, double t)
+double Rescaler::linearInterpolation(double s, double e, double t)
 {
 	return s + (e - s) * t;
 }
 
-double Rescaler::blerp(double c00, double c10, double c01, double c11, double tx, double ty)
+double Rescaler::bilinearInterpolation(double c00, double c10, double c01, double c11, double tx, double ty)
 {
-	return lerp(lerp(c00, c10, tx), lerp(c01, c11, tx), ty);
+	return linearInterpolation(linearInterpolation(c00, c10, tx), linearInterpolation(c01, c11, tx), ty);
+}
+
+void Rescaler::scaleImage(double scale_x, double scale_y)
+{
+	const auto new_width = static_cast<int>(oldImage->width * scale_x);
+	const auto new_height = static_cast<int>(oldImage->height * scale_y);
+	this->newImage->resize(new_width, new_height);
+
+	for (int x = 0; x < newImage->width; x++) {
+		for (int y = 0; y < newImage->height; y++)
+		{
+			const auto gx = static_cast<double>(x) / newImage->width * (oldImage->width - 1);
+			const auto gy = static_cast<double>(y) / newImage->height * (oldImage->height - 1);
+			auto gxi = static_cast<int>(gx);
+			auto gyi = static_cast<int>(gy);
+
+			// space for operation on pixels and their colors
+			// to be implemented
+			//
+		}
+	}
 }
--- graphic-file-converter/rescaler.h
+++ graphic-file-converter/rescaler.h
@@ -3,9 +3,33 @@
 
 class Rescaler : public Converter
 {
-private:
-	static double lerp(double s, double e, double t);
-	static double blerp(double c00, double c10, double c01, double c11, double tx, double ty);
+	/**
+	 * \brief 
+	 * \param s 
+	 * \param e 
+	 * \param t 
+	 * \return 
+	 */
+	static double linearInterpolation(double s, double e, double t);
+
+	/**
+	 * \brief Takes 4 points and uses the linear interpolation on them
+	 * \param c00 color of the first pixel
+	 * \param c10 color of the second pixel
+	 * \param c01 color of the third pixel
+	 * \param c11 color of the fourth pixel
+	 * \param tx Difference between gx and gx cast to int
+	 * \param ty Difference between gy and gy cast to int
+	 * \return Interpolated value
+	 */
+	static double bilinearInterpolation(double c00, double c10, double c01, double c11, double tx, double ty);
+
+	/**
+	 * \brief Scaling function implementation
+	 * \param scale_x New width is equal to old width multiplied by this constant
+	 * \param scale_y New height is equal to old width multiplied by this constant
+	 */
+	void scaleImage(double scale_x, double scale_y);
 
 	using Converter::Converter;
 };
