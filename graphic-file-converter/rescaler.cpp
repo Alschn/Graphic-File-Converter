faf3a83a81b856e463b7efd1d43e00973ece64c2
Adam Lisichin <01149732@pw.edu.pl>
2020-04-26 15:34:43 +0200

    Minor changes to rescaler code

--- graphic-file-converter/rescaler.cpp
+++ graphic-file-converter/rescaler.cpp
@@ -1,11 +1,11 @@
 #include "rescaler.h"
 
-double Lerp(double s, double e, double t)
+double Rescaler::lerp(double s, double e, double t)
 {
 	return s + (e - s) * t;
 }
 
-double Blerp(double c00, double c10, double c01, double c11, double tx, double ty)
+double Rescaler::blerp(double c00, double c10, double c01, double c11, double tx, double ty)
 {
-	return Lerp(Lerp(c00, c10, tx), Lerp(c01, c11, tx), ty)
+	return lerp(lerp(c00, c10, tx), lerp(c01, c11, tx), ty);
 }
--- graphic-file-converter/rescaler.h
+++ graphic-file-converter/rescaler.h
@@ -1,10 +1,11 @@
 #pragma once
+#include "converter.h"
 
 class Rescaler : public Converter
 {
 private:
-	static double Lerp(double s, double e, double t);
-	static double Blerp(double c00, double c10, double c01, double c11, double tx, double ty);
+	static double lerp(double s, double e, double t);
+	static double blerp(double c00, double c10, double c01, double c11, double tx, double ty);
 
 	using Converter::Converter;
 };
