730c827c99365ddd9866d675a90d3cbd180e45ca
Adam Lisichin <01149732@pw.edu.pl>
2020-05-02 12:02:02 +0200

    To be implemented

--- tests/testInterpolation.cpp
+++ tests/testInterpolation.cpp
@@ -1,2 +1,22 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "../graphic-file-converter/rescaler.h"
+#include "../graphic-file-converter/converter.h"
+
+//using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+//
+//namespace Tests
+//{
+//	TEST_CLASS(testReflection)
+//	{
+//		TEST_METHOD(testLerp1)
+//		{
+//
+//		}
+//
+//		TEST_METHOD(testBlerp1)
+//		{
+//
+//		}
+//	};
+//}
